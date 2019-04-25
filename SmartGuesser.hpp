104a2df1c098545364d05c32ee5e5a15b7dccb68
orenIsabella <33618982+orenIsabella@users.noreply.github.com>
2019-04-25 16:45:38 +0300

    Update SmartGuesser.hpp

--- SmartGuesser.hpp
+++ SmartGuesser.hpp
@@ -1,1 +1,17 @@
+#pragma once
+#include "Guesser.hpp"
+using std::string;
+#include <iostream>
+
+/**
+ *TheGuesser will find the number by 100 steps the most!
+ */
+class TheGuesser: public bullpgia::Guesser {
+	string guess() override;
+    public:
+
+    void startNewGame(uint length){}
+    void learn(string reply){}
+    
+};
 
