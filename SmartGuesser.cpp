fe74716c2a5cc615524de168a39d1f1f8713352b
orenIsabella <33618982+orenIsabella@users.noreply.github.com>
2019-04-25 16:43:20 +0300

    Update SmartGuesser.cpp

--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -1,1 +1,20 @@
+#include "SmartGuesser.hpp"
+#include <stdlib.h>
+#include <iostream>
 
+void learn(string reply) {
+    cout<<"sn=mart guesser cpp"<<endl;
+}
+
+void startNewGame(uint length) {
+    cout<<"Start new game (SmartGuesser cpp)"<<endl;
+}
+
+
+/**
+*TheGuesser will find the number by 100 steps the most!
+*/
+std::string TheGuesser::guess() {
+	std::string ans="";
+	return ans;
+}
