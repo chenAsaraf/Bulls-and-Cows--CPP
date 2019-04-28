e2dfad646ec1d76dcb03d3c6aeffce7f9d635c9b
Isabella Oren <orenisabella@gmail.com>
2019-04-28 11:36:44 +0300

    Added tests

--- Test.cpp
+++ Test.cpp
@@ -48,6 +48,37 @@ int main() {
 		for (uint i=0; i<100; ++i) {
 			testcase.CHECK_EQUAL(play(randy, smarty, 4, 100)<=10, true);  // smarty should always win in at most 10 turns!
 		}
+		//our tests:
+		testcase.setname("Calculate bull and pgia")
+		.CHECK_OUTPUT(calculateBullAndPgia("1235","1234"), "3,0")      // 3 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("5674","4567"), "0,4")      // 0 bull, 4 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("0000","9999"), "0,0")      // 0 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1234","3412"), "0,4")      // 0 bull, 4 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("5555","5555"), "4,0")      // 4 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1234","1134"), "3,1")      // 3 bull, 1 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("5634","5678"), "2,0")      // 2 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("5634","5643"), "2,2")      // 2 bull, 2 pgia
+		;
+
+		testcase.setname("Play with dummy choosers and guessers")
+		.CHECK_EQUAL(play(c45678, g45678, 5, 100), 1)      // guesser wins in one turn.
+		.CHECK_EQUAL(play(c45678111, g45678111, 8, 100), 1)      // guesser wins in one turn.
+		.CHECK_EQUAL(play(c1000, g9999, 4, 100), 101)    // guesser loses by running out of turns 
+		.CHECK_EQUAL(play(c11, g19, 2, 3), 4)    // guesser loses by running out of turns 
+		.CHECK_EQUAL(play(c123456789, g1234567890, 9, 100), 101)   // guesser loses technically by making an illegal guess (too long).
+		.CHECK_EQUAL(play(c98765, g9876, 4, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
+		;
+
+		testcase.setname("Play with smart guesser");
+		std::string r="";
+		char c = '0' + (9%10);
+		r += c;
+		SmartGuesser smarttest;
+		for (uint i=0; i<100; ++i) {
+			c = '0' + (i%10);
+			r += c;
+			testcase.CHECK_EQUAL(play(r, smarttest, 4, 100)<=10, true);  // smarttest should always win in at most 10 turns!
+		}
 
     grade = testcase.grade();
 	} else {
